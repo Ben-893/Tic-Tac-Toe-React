586a1e30886459272b5352e0d830f1cf98af4b2e
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-07-28 16:59:37 +0100

    Added a new state of xIsNext and a ternary operator to control a player's turn

--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Square from "./Square";
 
 class Board extends React.Component {
@@ -6,13 +6,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares: squares, 
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -25,7 +29,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
